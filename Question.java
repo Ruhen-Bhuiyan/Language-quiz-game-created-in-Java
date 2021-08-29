c1f013fb9ffa49e0d1896584f5446d91d06c0f0c
Ruhen-Bhuiyan <43878741+Ruhen-Bhuiyan@users.noreply.github.com>
2021-08-29 18:58:48 +0600

    Update Question.java

--- Question.java
+++ Question.java
@@ -5,7 +5,6 @@ public class Question implements ActionListener{
 	String[] questions = 	{
 				"How many definite articles are in German language?",
 				"How to say no in German?",
-		
 				"How to say yes in German?",
 				"How to say goodbye in German?"					
 							};
