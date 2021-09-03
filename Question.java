481a3e39dcb04560284cbd4ee41d47c6d554bb53
Ruhen-Bhuiyan <43878741+Ruhen-Bhuiyan@users.noreply.github.com>
2021-09-03 22:47:27 +0600

    Update Question.java

--- Question.java
+++ Question.java
@@ -1,7 +1,8 @@
 import java.awt.event.*;
 import java.awt.*;
 import javax.swing.*;
-public class Question implements ActionListener{	
+public class Question implements ActionListener{
+	
 	String[] questions = 	{
 				"How many definite articles are in German language?",
 				"How to say no in German?",
